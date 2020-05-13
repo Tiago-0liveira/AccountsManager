6594ad9eb200ee3d65137fb3e6e179a11002636f
Tiago Oliveira <tfgoliveira@gmail.com>
2020-05-13 22:53:50 +0100

    Update pass.bat

--- pass.bat
+++ pass.bat
@@ -1,2 +1,3 @@
 @echo off
-python 
+python (the directory that you will keep the file(the .py one)) for example, C:\Users\myprograms\python\passwordkeeper.py
+:: You can change this file name aswell.
